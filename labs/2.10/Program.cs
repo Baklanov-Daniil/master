dfe58c82ee20cca6c924f521d6dc2c71e60885ed
Baklanov-Daniil <daniilbaklanov55@gmail.com>
2024-10-02 19:19:51 +0600

    Program.cs

--- labs/2.10/Program.cs
+++ labs/2.10/Program.cs
@@ -1,70 +1,94 @@
 using System;
 
-namespace Program;
-
-class Program
+namespace Program
 {
-    static void Main(string[] args)
+
+    class Program
     {
-        //задание 1
+        static void Main(string[] args)
+        {
+            //задание 1
 
-        int cnt = 0;
-        int n = Convert.ToInt32(Console.ReadLine()) - 2;
-        int prev = Convert.ToInt32(Console.ReadLine());
-        int mid = Convert.ToInt32(Console.ReadLine());
-        int next;
-        for (int i = 0; i<n; i++) {
-            next = Convert.ToInt32(Console.ReadLine());
-            if (mid<prev && mid<next) {
-                cnt += 1;
+            int cnt = 0;
+            int n = Convert.ToInt32(Console.ReadLine()) - 2;
+            int prev = Convert.ToInt32(Console.ReadLine());
+            int mid = Convert.ToInt32(Console.ReadLine());
+            int next;
+            for (int i = 0; i < n; i++)
+            {
+                next = Convert.ToInt32(Console.ReadLine());
+                if (mid < prev && mid < next)
+                {
+                    cnt += 1;
+                }
+                prev = mid;
+                mid = next;
             }
-            prev = mid;
-            mid = next;
-        }
-        Console.WriteLine(cnt);
+            Console.WriteLine(cnt);
+            Console.ReadLine();
 
-        //задание 2
+            //задание 2
 
-        int n = Convert.ToInt32(Console.ReadLine());
-        int max1 = 0;
-        int max2 = 0;
-        for (int i = 0; i<n; i++) {
-            int next = Convert.ToInt32(Console.ReadLine());
-            max2 = Math.Max(max2, max1);
-            max1 = Math.Max(max1, next);
-        }
-        Console.WriteLine(max2);
-
-        // задание 3
-
-        int n = Convert.ToInt32(Console.ReadLine());
-        int cnt = 0;
-        int next;
-        for (int i = 0; i<n; i++) {
-         next = Math.Abs(Convert.ToInt32(Console.ReadLine()));
-            if (next%10==3) {
-                cnt+=1;
+            int n = Convert.ToInt32(Console.ReadLine());
+            int a = Convert.ToInt32(Console.ReadLine());
+            int b = Convert.ToInt32(Console.ReadLine());
+            int max1 = Math.Max(a, b);
+            int max2 = Math.Min(a, b);
+            for (int i = 0; i < n - 2; i++)
+            {
+                int next = Convert.ToInt32(Console.ReadLine());
+                if (max1 < next && next > max2)
+                {
+                    max2 = max1;
+                    max1 = next;
+                }
+                else if (next > max2)
+                {
+                    max2 = next;
+                }
             }
-        }
-        Console.WriteLine(cnt);
-        
-        // задание 4
+            Console.WriteLine(max2);
+            Console.ReadLine();
 
-        int n = Convert.ToInt32(Console.ReadLine());
-        bool flag = true;
-        for (int i = 0; i<n; i++) {
-            int next = Convert.ToInt32(Console.ReadLine());
-            if ((Math.Abs(next)%10==4 && next%2==0) == false) {
-                // если число оканчивается на 4, то оно четное
-                flag = false;
+            // задание 3
+
+            int n = Convert.ToInt32(Console.ReadLine());
+            int cnt = 0;
+            int next;
+            for (int i = 0; i < n; i++)
+            {
+                next = Math.Abs(Convert.ToInt32(Console.ReadLine()));
+                if (next % 10 == 3)
+                {
+                    cnt += 1;
+                }
             }
-        }
-        if (flag){
-            Console.WriteLine("Да");
-        }
-        else {
-            Console.WriteLine("Нет");
+            Console.WriteLine(cnt);
+            Console.ReadLine();
+
+            // задание 4
+
+            int n = Convert.ToInt32(Console.ReadLine());
+            bool flag = true;
+            for (int i = 0; i < n; i++)
+            {
+                int next = Convert.ToInt32(Console.ReadLine());
+                if (!(Math.Abs(next) % 10 == 4 && next % 2 == 0))
+                {
+                    // если число оканчивается на 4, то оно итак четное
+                    flag = false;
+                }
+            }
+            if (flag)
+            {
+                Console.WriteLine("Да");
+                Console.ReadLine();
+            }
+            else
+            {
+                Console.WriteLine("Нет");
+                Console.ReadLine();
+            }
         }
     }
-    
 }
