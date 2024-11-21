ac8522b2d98723fc72ba80c4ea937e9ae3750841
Baklanov-Daniil <daniilbaklanov55@gmail.com>
2024-11-21 14:30:45 +0600

    Add files via upload

---
+++ practices/olimpeaidnie/moloko.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _5_practic_work
+{
+    internal class Program
+    {
+            static void Main(string[] args)
+            {
+                float mn = 1000;
+                int company = 0;
+                float price = 0;
+
+                int N = Convert.ToInt32(Console.ReadLine());
+                for (int i = 0; i < N; i++)
+                {
+                    float x1 = float.Parse(Console.ReadLine());
+                    float y1 = float.Parse(Console.ReadLine());
+                    float z1 = float.Parse(Console.ReadLine());
+                    float x2 = float.Parse(Console.ReadLine());
+                    float y2 = float.Parse(Console.ReadLine());
+                    float z2 = float.Parse(Console.ReadLine());
+                    float c1 = float.Parse(Console.ReadLine());
+                    float c2 = float.Parse(Console.ReadLine());
+                    float S1 = 2 * (x1 * y1 + x1 * z1 + y1 * z1);
+                    float S2 = 2 * (x2 * y2 + x2 * z2 + y2 * z2);
+                    float V1 = x1 * y1 * z1;
+                    float V2 = x2 * y2 * z2;
+                    price = (S1 * c2 - S2 * c1) / (V2 * S1 - S2 * V1) * 1000;
+                    if (price < mn)
+                    {
+                        mn = price;
+                        company = i + 1;
+                    }
+                    Console.WriteLine();
+                }
+
+                Console.WriteLine($"{company} {Math.Round(mn, 2)}");
+                Console.ReadLine();
+            }
+        }
+    }
+
+
---
+++ practices/olimpeaidnie/zeilewarenie.cs
@@ -0,0 +1,82 @@
+﻿using System;
+
+namespace practic_work
+{
+    class Program
+    {
+        static string Mix(string[] args, string answer)
+        {
+            string str = "";
+            for (int i = 1; i < args.Length; i++)
+            {
+                str = str + args[i];
+            }
+            return answer + $"MX{str}XM";
+        }
+
+        static string Water(string[] args, string answer)
+        {
+            string str = "";
+            for (int i = 1; i < args.Length; i++)
+            {
+                str = str + args[i];
+            }
+            return answer + $"WT{str}TW";
+        }
+
+        static string Dust(string[] args, string answer)
+        {
+            string str = "";
+            for (int i = 1; i < args.Length; i++)
+            {
+                str = str + args[i];
+            }
+            return answer + $"DT{str}TD";
+        }
+        static string Fire(string[] args, string answer)
+        {
+            string str = "";
+            for (int i = 1; i < args.Length; i++)
+            {
+                str = str + args[i];
+            }
+            return answer + $"FR{str}RF";
+        }
+
+        static void Main(string[] args)
+        {
+            string answer = "";
+            string massive = "";
+            while (true)
+            {
+                string enter = Console.ReadLine();
+                if (enter == "END")
+                {
+                    break;
+                }
+                string[] text = enter.Split(" ");
+                switch (text[0])
+                {
+                    case "MIX":
+                        massive += Mix(text, answer) + " ";
+                        break;
+                    case "WATER":
+                        massive += Water(text, answer) + " ";
+                        break;
+                    case "DUST":
+                        massive += Dust(text, answer) + " ";
+                        break;
+                    case "FIRE":
+                        massive += Fire(text, answer) + " ";
+                        break;
+                }
+            }
+            Console.WriteLine(massive);
+            string[] magic = massive.Split(" ");
+            Console.WriteLine(magic[2]);
+
+            Type a = typeof(int);
+            Console.WriteLine(a);
+        }
+    }
+}
