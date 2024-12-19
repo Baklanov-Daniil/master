d1e9abbc317ab6e5445806a3cf62573f9c9557ca
Baklanov-Daniil <daniilbaklanov55@gmail.com>
2024-12-19 11:58:10 +0600

    Update Program.cs

--- labs/1.11/Program.cs
+++ labs/1.11/Program.cs
@@ -24,8 +24,11 @@ namespace lab
                 for (int j = 0; j < n; j++)
                 {
                     param[i, 0] += arr[i, j];
-                    if (arr[i, j] == 0) { param[i, 2] += 1; }
-                    else { param[i, 1] += arr[i, j]; }
+                    if (arr[i, j] == 0) 
+                    {
+                        param[i, 2] += 1; 
+                    }
+                    param[i, 1] += arr[i, j]; 
                 }
             }
             for (int i = 0; i < n; i++)
