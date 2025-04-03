1bd1746620967c614e5e83863f60c4109c20303b
Baklanov-Daniil <daniilbaklanov55@gmail.com>
2025-04-03 22:58:39 +0600

    02.13 удалил старый код

--- aip/second-grade/02.13/obj/Debug/net9.0/02.13.AssemblyInfo.cs
+++ aip/second-grade/02.13/obj/Debug/net9.0/02.13.AssemblyInfo.cs
@@ -10,12 +10,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("01.13")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("02.13")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fc21ac52d9d648ad126dd67db8dfcac2c5e22a48")]
-[assembly: System.Reflection.AssemblyProductAttribute("01.13")]
-[assembly: System.Reflection.AssemblyTitleAttribute("01.13")]
+[assembly: System.Reflection.AssemblyProductAttribute("02.13")]
+[assembly: System.Reflection.AssemblyTitleAttribute("02.13")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // Создано классом WriteCodeFragment MSBuild.
